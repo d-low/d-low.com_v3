3d3930be6af0441dad277e4dd3b075b2d43ed8ec
Mike DiLorenzo <mikedilorenzo@Mikes-MBP.hsd1.co.comcast.net>
2016-12-21 23:43:20 -0700

    Add webpack loaders. Remove trailing commas.

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,38 @@
 var path = require('path');
- 
+
 var config = {
   context: path.join(__dirname, 'src'),
   entry: [
-    './main.js',
+    './main.js'
   ],
   output: {
     path: path.join(__dirname, 'www'),
-    filename: 'bundle.js',
+    filename: 'bundle.js'
   },
   module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ['babel'],
-      },
-    ],
+    loaders: [{
+      test: /\.js$/,
+      include: __dirname + '/src',
+      loaders: ['babel']
+    }, {
+      test: /\.scss/,
+      // TBD: include: __dirname + '/src', 
+      loaders: ['style', 'css', 'sass']
+    }, {
+      test: /\.html/,
+      // TBD: include: __dirname + '/src', 
+      loader: 'html'
+    }]
   },
   resolveLoader: {
     root: [
-      path.join(__dirname, 'node_modules'),
-    ],
+      path.join(__dirname, 'node_modules')
+    ]
   },
   resolve: {
     root: [
-      path.join(__dirname, 'node_modules'),
-    ],
-  },
+      path.join(__dirname, 'node_modules')
+    ]
+  }
 };
 module.exports = config;
