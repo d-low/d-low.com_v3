aa0432d136af384f43ce77213d85a64a29bc5013
Mike DiLorenzo <mikedilorenzo@Mikes-MBP.hsd1.co.comcast.net>
2016-12-21 23:54:23 -0700

    Add site header component.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SiteHeaderNavigation from './site-header-navigation';
 import SiteFooterLinks from './site-footer-links';
 
 document.addEventListener('DOMContentLoaded', function() {
+  ReactDOM.render(
+    React.createElement(SiteHeaderNavigation),
+    document.querySelector('.js-site-header-nav')
+  );
   ReactDOM.render(
     React.createElement(SiteFooterLinks),
     document.querySelector('.js-site-footer')
