5199c6725525bd443d7aab7aa8b83646d535833a
Mike DiLorenzo <mikedilorenzo@gmail.com>
2016-12-28 22:50:53 -0700

    Add webpack file and image loads.

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,12 @@ var config = {
       test: /\.html/,
       // TBD: include: __dirname + '/src', 
       loader: 'html'
+    }, {
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      loaders: [
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?optimizationLevel=7&interlaced=false'
+      ]
     }]
   },
   resolveLoader: {
