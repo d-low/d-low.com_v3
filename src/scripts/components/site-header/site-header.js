d7b26c5215d87d7230b7c835a97e23ebc9a2112f
Mike DiLorenzo <mikedilorenzo@gmail.com>
2017-01-07 21:59:14 -0700

    Fade out logo when header scrolled out of view. Need to handle resize!

--- src/scripts/components/site-header/site-header.js
+++ src/scripts/components/site-header/site-header.js
@@ -4,15 +4,84 @@ import styles from './site-header.css';
 class SiteHeader extends React.Component {
   constructor() {
     super();
+
+    this.didScroll = false;
+    this.logo = null;
+    this.scrollInterval = null;
+    this.scrollStopTimeout = null;
+
+    this.checkIfScrolled = this.checkIfScrolled.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
+    this.handleScrollStop = this.handleScrollStop.bind(this);
+  }
+
+  componentDidMount() {
+    this.setLogoOpacity();
+
+    // When the logo is fixed on larger screens then we want to fade it out
+    // when the site header is scrolled out of view.
+    
+    if (window.getComputedStyle(this.logo).position === 'fixed') {
+      window.addEventListener('scroll', this.handleScroll);
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    this.didScroll = true;
+
+    if (!this.scrollInterval) {
+      this.checkIfScrolled();
+      this.scrollInterval = window.setInterval(this.checkIfScrolled, 16);
+    }
+  }
+
+  handleScrollStop() {
+    window.clearInterval(this.scrollInterval);
+    this.scrollInterval = null;
+  }
+
+  checkIfScrolled() {
+    if (this.didScroll) {
+      window.requestAnimationFrame(this.setLogoOpacity.bind(this));
+      window.clearTimeout(this.scrollStopTimeout);
+      this.scrollStopTimeout = window.setTimeout(this.handleScrollStop, 100);
+
+      this.didScroll = false;
+    }
+  }
+
+  setLogoOpacity() {
+    const logoHeight = parseInt(window.getComputedStyle(this.logo).height, 10);
+    let opacity = Number(1 - (window.pageYOffset / (window.innerHeight - logoHeight))).toFixed(2);
+
+    opacity = opacity < 0 ? 0 : opacity;
+    this.logo.style.opacity = opacity;
+
+    // The logo needs to be hidden (dislay: none) when it is no longer visible, 
+    // when content below it has been scrolled into view, so that it doesn't 
+    // sit on top of now visible content preventing the user from interacting 
+    // with it.
+
+    if (opacity === 0) {
+      this.logo.style.display = 'none';
+    }
+    else {
+      this.logo.style.display = 'block';
+    }
   }
 
   render() {
+
     // TODO: When not on the home page then use siteHeaderContentPage and
     // logoContentPage
 
     return (  
       <header className={styles.siteHeaderHomePage}>
-        <div className={styles.logo}>
+        <div className={styles.logo} ref={(el) => { this.logo = el; }}>
           <h1 className={styles.logoBigText}>
             d-low.com
           </h1>
