8a0ae300d61e5149def0e6a57169503fa1079178
Mike DiLorenzo <mikedilorenzo@gmail.com>
2017-02-04 22:20:48 -0700

    Site navigation styles

--- src/scripts/components/site-navigation/site-navigation.js
+++ src/scripts/components/site-navigation/site-navigation.js
@@ -1,23 +1,48 @@
 import React from 'react';
+import styles from './site-navigation.css';
 
-const SiteNavigation = function SiteNavigation(props) {
-  const listItems = props.links.map(link =>
-    <li key={link.name}>
-      <a href={link.href}>{ link.name }</a>
-    </li>,
-  );
+class SiteNavigation extends React.Component {
+  constructor() {
+    super();
 
-  return (
-    <nav>
-      <button>
-        <i className="icon icon-hamburger" />
-      </button>
-      <ul>
-        {listItems}
-      </ul>
-    </nav>
-  );
-};
+    this.state = {
+      expanded: false,
+    };
+
+    this.toggleSiteNavigation = this.toggleSiteNavigation.bind(this);
+  }
+
+  toggleSiteNavigation() {
+    this.setState({ expanded: !this.state.expanded });
+  }
+
+  render() {
+    const listItems = this.props.links.map(link =>
+      <li className={styles.link} key={link.name}>
+        <a className={styles.linkText} href={link.href}>{ link.name }</a>
+      </li>,
+    );
+
+    const linksClassName = this.state.expanded ?
+      styles.linksExpanded : styles.links;
+    const closeButtonClassName = this.state.expanded ?
+      styles.closeButtonExpanded : styles.closeButton;
+
+    return (
+      <nav>
+        <button className={styles.actionButton} onClick={this.toggleSiteNavigation}>
+          <i className={styles.actionButtonIcon} />
+        </button>
+        <button className={closeButtonClassName} onClick={this.toggleSiteNavigation}>
+          <i className={styles.closeButtonIcon} />
+        </button>
+        <ul className={linksClassName}>
+          {listItems}
+        </ul>
+      </nav>
+    );
+  }
+}
 
 SiteNavigation.propTypes = {
   links: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
