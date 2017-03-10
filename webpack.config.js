bc8175353e5b32dfdb830788ea0815975c982a0a
Mike DiLorenzo <mikedilorenzo@gmail.com>
2017-03-09 23:58:46 -0700

    Initial upgrade to webpack 2. Need to review a few items still...

--- webpack.config.js
+++ webpack.config.js
@@ -3,79 +3,107 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 
+// TODO:
+// 1) JS source maps are off by a few lines
+// 2) Enable:
+//    - autoprefixer via postcss
+//    - Promise and fetch polyfills
+//    - Image optimization (maybe?)
+// 3) Production build
+// 4) Does ESLINT work?
+
 // Loaders for CSS modules
 const cssLoaders = [
-  'css-loader?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]',
-  'postcss-loader'
+  {
+    loader: 'css-loader',
+    options: {
+      sourceMap: true,
+      modules: true,
+      importLoaders: 1,
+      localIdentName: '[name]__[local]'
+    }
+  },
+  // {
+  //   loader: 'postcss-loader',
+  //   options: {
+  //     autoprefixer: {
+  //       browsers: ['last 2 versions']
+  //     }
+  //   }
+  // }
 ];
 
 const config = {
   entry: {
-    app: './src/scripts/main.js'
+    app: './src/scripts/main.js',
   },
   output: {
     filename: '[name].js',
-    path: path.join(__dirname, 'www')
+    path: path.join(__dirname, 'www'),
   },
   module: {
-    preLoaders: [{
-      test: /\.js$/, 
-      include: __dirname + '/src/scripts',
-      loader: 'eslint-loader', 
-      exclude: /node_modules/
-    }],
-    loaders: [{
-      test: /\.js$/,
-      include: __dirname + '/src/scripts',
-      loaders: ['babel']
-    }, { 
-      test: /\.css$/, 
-      // include: __dirname + '/src',
-      loader: ExtractTextPlugin.extract('style-loader', cssLoaders.join('!'))
-    }, {
-      test: /\.html/,
-      // TBD: include: __dirname + '/src', 
-      loader: 'html'
-    }, {
-      test: /\.(eot|svg|ttf|woff|woff2)$/,
-      loader: 'url-loader',
-      options: {
-        limit: 50000
-      }
-    }, {
-      test: /\.(jpe?g|png|gif|svg)$/i,
-      loaders: [
-        'file?hash=sha512&digest=hex&name=[hash].[ext]',
-        'image-webpack?optimizationLevel=7&interlaced=false'
-      ]
-    }]
-  },
-  eslint: {
-    configFile: './.eslintrc'
+    rules: [
+      {
+        test: /\.js$/, 
+        enforce: 'pre',
+        include: __dirname + '/src/scripts',
+        loader: 'eslint-loader', 
+        exclude: /node_modules/,
+        options: {
+          configFile: './.eslintrc',
+        },
+      },
+      {
+        test: /\.js$/,
+        include: __dirname + '/src/scripts',
+        loader: 'babel-loader',
+      }, 
+      { 
+        test: /\.css$/, 
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          loader: cssLoaders,
+        }),
+      }, 
+      {
+        test: /\.html/,
+        loader: 'html-loader',
+      }, 
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 50000,
+        },
+      }, 
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'file-loader',
+        options: {
+          hash: 'sha512',
+          digest: 'hex',
+          name: '[hash].[ext]',
+        },
+      },
+    ],
   },
   plugins: [
-    new ExtractTextPlugin('[name].css', {
-      ignoreOrder: true
+    new ExtractTextPlugin({
+      filename: '[name].css',
+      ignoreOrder: true,
     }),
-    new webpack.ProvidePlugin({
-      Promise: 'imports?this=>global!exports?global.Promise!es6-promise',
-      fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch'
-    })
+    // new webpack.ProvidePlugin({
+    //   Promise: 'imports-loader?this=>global!exports?global.Promise!es6-promise',
+    //   fetch: 'imports-loader?this=>global!exports?global.fetch!whatwg-fetch'
+    // })
   ],
-  postcss: [
-    autoprefixer({
-      browsers: ['last 2 versions']
-    })
-  ],
-  resolveLoader: {
-    root: [
-      path.join(__dirname, 'node_modules')
-    ]
-  },
   resolve: {
-    extensions: ['', '.css', '.js'],
-    root: [path.join(__dirname, './src')]
-  }
+    extensions: ['.css', '.js'],
+    modules: [
+      path.join(__dirname, 'src'),
+      'node_modules',
+    ],
+  },
 };
 
 module.exports = config;
