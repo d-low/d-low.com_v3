93ec53cbc14068c27c0745180c12ceb6dd2e7b5d
Mike DiLorenzo <mikedilorenzo@gmail.com>
2017-03-19 23:55:00 -0600

    Add initial image slider elements and styles. Much more to come!

--- src/scripts/components/image-slider/image-slider.js
+++ src/scripts/components/image-slider/image-slider.js
@@ -1,3 +1,5 @@
+/* eslint react/no-unused-prop-types: 0 */
+
 import React from 'react';
 import styles from './image-slider.css';
 
@@ -6,30 +8,24 @@ import styles from './image-slider.css';
  * Image Slider.
  * @see https://github.com/Codeinwp/Ideal-Image-Slider-JS
  */
-class ImageSlider extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      currentImage: this.props.currentImage,
-      visible: this.props.visible,
-    };
-  }
-
+class ImageSlider extends React.PureComponent {
   // /**
   //  * TODO:
   //  * 1) If visible then load the current, previous and next images
   //  */
   // componentDidMount() {
-  //   if (this.state.visible) {
+  //   if (this.props.visible) {
   //   }
   // }
 
   render() {
-    const containerClassName = this.state.visible ? styles.containerVisible : styles.container;
+    const containerClassName = this.props.visible ? styles.containerVisible : styles.container;
     const listItems = this.props.images.map(image =>
       <li className={styles.item} key={image.href}>
-        <figure className={styles.image} data-image={image.href}>
+        <figure
+          className={styles.image}
+          data-image={image.href}
+          style={{ backgroundImage: `url(${image.href})` }}>
           <figcaption className={styles.caption}>
             {image.caption}
           </figcaption>
