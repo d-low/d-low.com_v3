fce416d41d4511c5fecae7cb4b4e257ffb2087df
Mike DiLorenzo <mikedilorenzo@gmail.com>
2017-03-22 00:11:41 -0600

    Add swipe navigation to image slider using Hammer.js

--- src/scripts/components/image-slider/image-slider.js
+++ src/scripts/components/image-slider/image-slider.js
@@ -1,6 +1,7 @@
-/* eslint react/no-unused-prop-types: 0 */
+/* eslint react/no-unused-prop-types: 0, no-console: 0, class-methods-use-this: 0 */
 
 import React from 'react';
+import Hammer from 'react-hammerjs';
 import styles from './image-slider.css';
 
 /**
@@ -8,15 +9,43 @@ import styles from './image-slider.css';
  * Image Slider.
  * @see https://github.com/Codeinwp/Ideal-Image-Slider-JS
  */
-class ImageSlider extends React.PureComponent {
+class ImageSlider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentImage: props.currentImage,
+    };
+
+    this.handleSwipe = this.handleSwipe.bind(this);
+  }
+
   // /**
   //  * TODO:
   //  * 1) If visible then load the current, previous and next images
+  //  * 2) Then show the slider
   //  */
   // componentDidMount() {
   //   if (this.props.visible) {
   //   }
   // }
+
+  /**
+   * @todo Note that the Hammer.DIRECTION_LEFT and Hammer.DIRECTION_RIGHT
+   * constants are not available on the React Hammer component so we've hard
+   * coded their values, 2 and 4, here.
+   */
+  handleSwipe(e) {
+    if (e.direction === 2 && this.state.currentImage + 1 < this.props.images.length - 1) {
+      this.setState({
+        currentImage: this.state.currentImage + 1,
+      });
+    } else if (e.direction === 4 && this.state.currentImage - 1 >= 0) {
+      this.setState({
+        currentImage: this.state.currentImage - 1,
+      });
+    }
+  }
 
   render() {
     const containerClassName = this.props.visible ? styles.containerVisible : styles.container;
@@ -35,9 +64,13 @@ class ImageSlider extends React.PureComponent {
 
     return (
       <div className={containerClassName}>
-        <ul className={styles.items}>
-          {listItems}
-        </ul>
+        <Hammer onSwipe={this.handleSwipe}>
+          <ul
+            className={styles.items}
+            style={{ transform: `translateX(${this.state.currentImage * -100}vw)` }}>
+            {listItems}
+          </ul>
+        </Hammer>
       </div>
     );
   }
